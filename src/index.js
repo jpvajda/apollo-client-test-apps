17819146df545be947087ce7a3b63a17e1b4d4eb
John P. Vajda <jvajda@apollographql.com>
2022-04-13 18:13:59 -0600

    feat: added app examples

--- src/index.js
+++ src/index.js
@@ -1,17 +1,127 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+  ApolloLink,
+} from "@apollo/client";
+import { createHttpLink } from "apollo-link-http";
+import { MultiAPILink } from "@habx/apollo-multi-endpoint-link";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// uses  https://github.com/habx/apollo-multi-endpoint-link to fetch from
+// many endpoints
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([
+    new MultiAPILink({
+      endpoints: {
+        exchanges: "https://48p1r2roz4.sse.codesandbox.io",
+        dogs: "https://71z1g.sse.codesandbox.io/",
+      },
+      createHttpLink: () => createHttpLink(),
+    }),
+  ]),
+});
+
+//Exchange Rate Code Start
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates @api(contextKey: exchanges) {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
+//Exchange Rate Code End
+
+// Dog Code Start
+
+const GET_DOGS = gql`
+  query GetDogs @api(name: dogs) {
+    dogs {
+      id
+      breed
+    }
+  }
+`;
+
+function Dogs({ onDogSelected }) {
+  const { loading, error, data } = useQuery(GET_DOGS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <select name="dog" onChange={onDogSelected}>
+      {data.dogs.map((dog) => (
+        <option key={dog.id} value={dog.breed}>
+          {dog.breed}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+const GET_DOG_PHOTO = gql`
+  query Dog($breed: String!) @api(name: dogs) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`;
+
+function DogPhoto({ breed }) {
+  const { loading, error, data } = useQuery(GET_DOG_PHOTO, {
+    variables: { breed },
+    pollInterval: 500,
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return (
+    <img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
+  );
+}
+
+// Dog Code End
+
+function App() {
+  return (
+    <div>
+      <h2>Exchange Rates 💰 </h2>
+      <ExchangeRates />
+      <h2>Dogs 🐕 </h2>
+      <Dogs />
+      <DogPhoto />
+    </div>
+  );
+}
+
+render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
